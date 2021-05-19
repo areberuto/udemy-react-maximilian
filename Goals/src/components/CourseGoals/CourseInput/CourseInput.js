c8c545afb3ce2b061a5b433af9e49379bc07fdb6
areberuto <albertopr93@gmail.com>
2021-05-19 18:59:27 +0200

    feat: finished styling section (6)

--- Goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,16 +1,61 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
 
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  & ~ Button {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    & ~ Button {
+      width: auto;
+    }
+  }
+
+  ${(props) =>
+    props.invalid &&
+    `
+    & input {
+      background: rgba(255, 0, 0, 0.3);
+    }
+    & input, & label {
+      color: red;
+    }
+  `}
+`;
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    if (enteredValue.trim().length) {
+    if (event.target.value.trim().length !== 0) {
       setIsValid(true);
-      return;
     }
     setEnteredValue(event.target.value);
   };
@@ -26,20 +71,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: isValid ? "black" : "red" }}>Course Goal</label>
-        <input
-          type="text"
-          style={{
-            borderColor: isValid ? "" : "red",
-            backgroundColor: isValid ? "" : "rgba(255, 0, 0, 0.3)",
-          }}
-          onChange={goalInputChangeHandler}
-        />
-        {!isValid && (
-          <p className="course-input__warn">Oi! Write something first.</p>
-        )}
-      </div>
+      <FormControl aria-label="yes" invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
